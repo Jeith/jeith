f73bcf2b413bbfd68462624f397d16fc1bf3ab61
Jeith <keithp1208@yahoo.com>
2021-02-13 00:03:42 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const createError = require('http-errors')
+const express = require('express')
+const Raven = require('raven')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+const app = express()
+
+
+const indexRouter = require('./routes/index')
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.text())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+//= =====ROUTES=============
+app.use('/', indexRouter)
+
+//= =====================================
+//= ===========ERROR HANDLERS============
+//= =====================================
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+const port = process.env.PORT || 1334
+
+app.listen(port, () => {
+  console.log(`🚀 Listening on Port ${port}!`)
+  console.log(path.join(__dirname, 'views'))
+})
+
+module.exports = app
