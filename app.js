9d8498c96615d2485888611f9ea2bca885cbb4c0
Jeith <keithp1208@yahoo.com>
2021-06-05 02:18:30 -0500

    fixing responsiveness on h2s in about section

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-const createError = require('http-errors')
 const express = require('express')
-const Raven = require('raven')
 const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 
@@ -14,25 +10,14 @@ const indexRouter = require('./routes/index')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.text())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //= =====ROUTES=============
 app.use('/', indexRouter)
-
-//= =====================================
-//= ===========ERROR HANDLERS============
-//= =====================================
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404))
-})
 
 const port = process.env.PORT || 1333
 
